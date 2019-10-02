6526e5bb0dcf57513185e03cbbc88cc38658fee7
julian <jtallar@itba.edu.ar>
2019-10-02 19:44:46 -0300

    Added some configPanels

--- components/test.js
+++ components/test.js
@@ -4,12 +4,6 @@ var app = new Vue({
     el: '#app',
     vuetify: new Vuetify(),
     data: {
-      active_tab: 1,
-      tabs: [
-        {index: 0, name: 'Home', dir: 'home.html'},
-        {index: 1, name: 'Rooms', dir: 'rooms.html'},
-        {index: 2, name: 'Devices', dir: '#two'},
-        {index: 3, name: 'Routines', dir: '#tree'}
-      ]
-    }
+      color: '#FFF100',
+    },
   })
