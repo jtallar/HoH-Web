7af2295956dedafa15aefab389016a0fb48844be
JulianVuoso <juvuoso@itba.edu.ar>
2019-10-02 19:43:19 -0300

    add

--- components/test.js
+++ components/test.js
@@ -4,6 +4,39 @@ var app = new Vue({
     el: '#app',
     vuetify: new Vuetify(),
     data: {
+      items: [
+        {
+          src: 'bedroom_01.jpg',
+        },
+        {
+          src: 'bathroom_02.jpg',
+        },
+        {
+          src: 'game_room_01.jpg',
+        },
+        {
+          src: 'garage_01.jpg',
+        },
+        {
+          src: 'kitchen_01.jpg',
+        },
+        {
+          src: 'living_01.jpg',
+        },
+        {
+          src: 'living_02.jpg',
+        },
+        {
+          src: 'entertainement_01.jpg',
+        },
+        {
+          src: 'kitchen1.jpg',
+        },
+      ],
+      overlay: false,
+      snackbarCan: false,
+      snackbarOk: false,
+      sheet: false,
       active_tab: 1,
       tabs: [
         {index: 0, name: 'Home', dir: 'home.html'},
