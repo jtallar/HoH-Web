abd30de895b632c8f3f1c3e35168217b17d0295d
julian <jtallar@itba.edu.ar>
2019-10-06 20:49:09 -0300

    dad

--- source/api.js
+++ source/api.js
@@ -1,152 +1,220 @@
-var api = class {
-    static get baseUrl() {
-        return "http://127.0.0.1:8080/api/";
-    }
+var Api = class {
+  static get baseUrl() {
+    return "http://127.0.0.1:8080/api/";
+  }
 
-    static get timeout() {
-        return 10000; // 10 seg
-    }
+  static get timeout() {
+    return 10000; // 10 seg
+  }
 
-    static fetch(url, init) {
-        return new Promise((solve, reject) => {
-            var timeout = setTimeout(() => {
-                reject(new Error('Time out'));
-            }, api.timeout);
+  static fetch(url, init) {
+    return new Promise((solve, reject) => {
+      var timeout = setTimeout(() => {
+        reject(new Error('Time out'));
+      }, api.timeout);
 
-            fetch(url, init)
-                .then((response) => {
-                    clearTimeout(timeout);
-                    if (!response.ok) {
-                        reject(new Error(response.statusText));
-                    }
-                    return response.json();
-                })
-                .then((data) => {
-                    solve(data);
-                })
-                .catch((error) => {
-                    reject(error);
-                });
+      fetch(url, init)
+        .then((response) => {
+          clearTimeout(timeout);
+          if (!response.ok) {
+            reject(new Error(response.statusText));
+          }
+          return response.json();
+        })
+        .then((data) => {
+          solve(data);
+        })
+        .catch((error) => {
+          reject(error);
         });
-    }
+    });
+  }
 }
 
 api.room = class {
-    static get url() {
-        return api.baseUrl + "rooms/";
-    }
+  static get url() {
+    return api.baseUrl + "rooms/";
+  }
 
-    static add(room) {
-        return api.fetch(api.room.url, {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json; charset=utf-8'
-            },
-            body: JSON.stringify(room)
-        });
-    }
+  /* Create a new room */
+  static add(room) {
+    return api.fetch(api.room.url, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json; charset=utf-8'
+      },
+      body: JSON.stringify(room)
+    });
+  }
 
-    static modify(room) {
-        return api.fetch(api.room.url + room.id, {
-            method: 'PUT',
-            headers: {
-                'Content-Type': 'application/json; charset=utf-8'
-            },
-            body: JSON.stringify(room)
-        });
-    }
+  /* Update a specific room */
+  static modify(room) {
+    return api.fetch(api.room.url + room.id, {
+      method: 'PUT',
+      headers: {
+        'Content-Type': 'application/json; charset=utf-8'
+      },
+      body: JSON.stringify(room)
+    });
+  }
 
-    static delete(id) {
-        return api.fetch(api.room.url + id, {
-            method: 'DELETE',
-        });
-    }
+  /* Delete an existing room */
+  static delete(room) {
+    return api.fetch(api.room.url + room.id, {
+      method: 'DELETE',
+    });
+  }
 
-    static get(id) {
-        return api.fetch(api.room.url + id);
-    }
+  /* Retrieve all rooms */
+  static getAll() {
+    return api.fetch(api.room.url);
+  }
 
-    static getAll() {
-        return api.fetch(api.room.url);
-    }
+  /* Retrieve DEVICES from a specific room */
+  static getDevices(room) {
+    return api.fetch(api.room.url + room.id + '/devices');
+  }
+
+  // To create a device, create device and then add to room
+  // Para que es el body??
+  /* Adds a device to a specific room */
+  static addDevice(room, device, param) {
+    return api.fetch(api.room.url + room.id + '/devices/' + device.id, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json; charset=utf-8'
+      },
+      body: JSON.stringify((param === undefined) ? "{}" : param)
+    });
+  }
+  
 }
 
 api.device = class {
   static get url() {
-      return api.baseUrl + "devices/";
+    return api.baseUrl + "devices/";
   }
 
+  /* Create a new device */
   static add(device) {
-      return api.fetch(api.device.url, {
-          method: 'POST',
-          headers: {
-              'Content-Type': 'application/json; charset=utf-8'
-          },
-          body: JSON.stringify(device)
-      });
+    return api.fetch(api.device.url, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json; charset=utf-8'
+      },
+      body: JSON.stringify(device)
+    });
   }
 
+  /* Update an existing device */
   static modify(device) {
-      return api.fetch(api.device.url + device.id, {
-          method: 'PUT',
-          headers: {
-              'Content-Type': 'application/json; charset=utf-8'
-          },
-          body: JSON.stringify(device)
-      });
+    return api.fetch(api.device.url + device.id, {
+      method: 'PUT',
+      headers: {
+        'Content-Type': 'application/json; charset=utf-8'
+      },
+      body: JSON.stringify(device)
+    });
   }
 
-  static delete(id) {
-      return api.fetch(api.device.url + id, {
-          method: 'DELETE',
-      });
+  /* Delete an existing device */
+  static delete(device) {
+    return api.fetch(api.device.url + device.id, {
+      method: 'DELETE',
+    });
   }
 
-  static get(id) {
-      return api.fetch(api.device.url + id);
+  /* Retrieve state from a specific device */
+  static getState(device) {
+    return api.fetch(api.device.url + device.id + '/state');
   }
 
+  /* Retrieve events from a specific device */
+  static getEvents(device) {
+    return api.fetch(api.device.url + device.id + '/events');
+  }
+
+  /* Retrieve all devices */
   static getAll() {
-      return api.fetch(api.device.url);
+    return api.fetch(api.device.url);
+  }
+
+  /* Retrieve devices from a specific deviceType */
+  static getAllFromType(deviceType) {
+    return api.fetch(api.device.url + 'devicetypes/' + deviceType.id);
+  }
+
+  /* Executes an action in a specific device with params {[]} */
+  static execAction(device, action, param) {
+    return api.fetch(api.device.url + device.id + '/' + action, {
+      method: 'PUT',
+      headers: {
+        'Content-Type': 'application/json; charset=utf-8'
+      },
+      body: JSON.stringify((param === undefined) ? "{}" : param)
+    });
+  }
+}
+
+api.deviceType = class {
+  static get url() {
+    return api.baseUrl + "devicestypes/";
+  }
+
+  /* Retrieves all deviceTypes */
+  static getAllTypes() {
+    return api.fetch(api.deviceType.url);
   }
 }
 
 api.routine = class {
   static get url() {
-      return api.baseUrl + "routines/";
+    return api.baseUrl + "routines/";
   }
 
+  /* Create a new routine */
   static add(routine) {
-      return api.fetch(api.routine.url, {
-          method: 'POST',
-          headers: {
-              'Content-Type': 'application/json; charset=utf-8'
-          },
-          body: JSON.stringify(routine)
-      });
+    return api.fetch(api.routine.url, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json; charset=utf-8'
+      },
+      body: JSON.stringify(routine)
+    });
   }
 
+  /* Update an existing routine */
   static modify(routine) {
-      return api.fetch(api.routine.url + routine.id, {
-          method: 'PUT',
-          headers: {
-              'Content-Type': 'application/json; charset=utf-8'
-          },
-          body: JSON.stringify(routine)
-      });
+    return api.fetch(api.routine.url + routine.id, {
+      method: 'PUT',
+      headers: {
+        'Content-Type': 'application/json; charset=utf-8'
+      },
+      body: JSON.stringify(routine)
+    });
   }
 
-  static delete(id) {
-      return api.fetch(api.routine.url + id, {
-          method: 'DELETE',
-      });
+  /* Delete an existing routine */
+  static delete(routine) {
+    return api.fetch(api.routine.url + routine.id, {
+      method: 'DELETE',
+    });
   }
 
-  static get(id) {
-      return api.fetch(api.routine.url + id);
-  }
-
+  /* Retrieve all routines */
   static getAll() {
-      return api.fetch(api.routine.url);
+    return api.fetch(api.routine.url);
+  }
+
+  /* Executes a specific routine */
+  // Para que es el body??
+  static exec(routine, param) {
+    return api.fetch(api.routine.url + routine.id + '/execute', {
+      method: 'PUT',
+      headers: {
+        'Content-Type': 'application/json; charset=utf-8'
+      },
+      body: JSON.stringify((param === undefined) ? "{}" : param)
+    });
   }
 }
--- source/components.js
+++ source/components.js
@@ -1,4 +1,226 @@
 // javascript file for used components
+var api = class {
+  static get baseUrl() {
+    return "http://127.0.0.1:8080/api/";
+  }
+
+  static get timeout() {
+    return 10000; // 10 seg
+  }
+
+  static fetch(url, init) {
+    return new Promise((solve, reject) => {
+      var timeout = setTimeout(() => {
+        reject(new Error('Time out'));
+      }, api.timeout);
+
+      fetch(url, init)
+        .then((response) => {
+          clearTimeout(timeout);
+          if (!response.ok) {
+            reject(new Error(response.statusText));
+          }
+          return response.json();
+        })
+        .then((data) => {
+          solve(data);
+        })
+        .catch((error) => {
+          reject(error);
+        });
+    });
+  }
+}
+
+api.room = class {
+  static get url() {
+    return api.baseUrl + "rooms/";
+  }
+
+  /* Create a new room */
+  static add(room) {
+    return api.fetch(api.room.url, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json; charset=utf-8'
+      },
+      body: JSON.stringify(room)
+    });
+  }
+
+  /* Update a specific room */
+  static modify(room) {
+    return api.fetch(api.room.url + room.id, {
+      method: 'PUT',
+      headers: {
+        'Content-Type': 'application/json; charset=utf-8'
+      },
+      body: JSON.stringify(room)
+    });
+  }
+
+  /* Delete an existing room */
+  static delete(room) {
+    return api.fetch(api.room.url + room.id, {
+      method: 'DELETE',
+    });
+  }
+
+  /* Retrieve all rooms */
+  static getAll() {
+    return api.fetch(api.room.url);
+  }
+
+  /* Retrieve DEVICES from a specific room */
+  static getDevices(room) {
+    return api.fetch(api.room.url + room.id + '/devices');
+  }
+
+  // To create a device, create device and then add to room
+  // Para que es el body??
+  /* Adds a device to a specific room */
+  static addDevice(room, device, param) {
+    return api.fetch(api.room.url + room.id + '/devices/' + device.id, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json; charset=utf-8'
+      },
+      body: JSON.stringify((param === undefined) ? "{}" : param)
+    });
+  }
+  
+}
+
+api.device = class {
+  static get url() {
+    return api.baseUrl + "devices/";
+  }
+
+  /* Create a new device */
+  static add(device) {
+    return api.fetch(api.device.url, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json; charset=utf-8'
+      },
+      body: JSON.stringify(device)
+    });
+  }
+
+  /* Update an existing device */
+  static modify(device) {
+    return api.fetch(api.device.url + device.id, {
+      method: 'PUT',
+      headers: {
+        'Content-Type': 'application/json; charset=utf-8'
+      },
+      body: JSON.stringify(device)
+    });
+  }
+
+  /* Delete an existing device */
+  static delete(device) {
+    return api.fetch(api.device.url + device.id, {
+      method: 'DELETE',
+    });
+  }
+
+  /* Retrieve state from a specific device */
+  static getState(device) {
+    return api.fetch(api.device.url + device.id + '/state');
+  }
+
+  /* Retrieve events from a specific device */
+  static getEvents(device) {
+    return api.fetch(api.device.url + device.id + '/events');
+  }
+
+  /* Retrieve all devices */
+  static getAll() {
+    return api.fetch(api.device.url);
+  }
+
+  /* Retrieve devices from a specific deviceType */
+  static getAllFromType(deviceType) {
+    return api.fetch(api.device.url + 'devicetypes/' + deviceType.id);
+  }
+
+  /* Executes an action in a specific device with params {[]} */
+  static execAction(device, action, param) {
+    return api.fetch(api.device.url + device.id + '/' + action, {
+      method: 'PUT',
+      headers: {
+        'Content-Type': 'application/json; charset=utf-8'
+      },
+      body: JSON.stringify((param === undefined) ? "{}" : param)
+    });
+  }
+}
+
+api.deviceType = class {
+  static get url() {
+    return api.baseUrl + "devicestypes/";
+  }
+
+  /* Retrieves all deviceTypes */
+  static getAllTypes() {
+    return api.fetch(api.deviceType.url);
+  }
+}
+
+api.routine = class {
+  static get url() {
+    return api.baseUrl + "routines/";
+  }
+
+  /* Create a new routine */
+  static add(routine) {
+    return api.fetch(api.routine.url, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json; charset=utf-8'
+      },
+      body: JSON.stringify(routine)
+    });
+  }
+
+  /* Update an existing routine */
+  static modify(routine) {
+    return api.fetch(api.routine.url + routine.id, {
+      method: 'PUT',
+      headers: {
+        'Content-Type': 'application/json; charset=utf-8'
+      },
+      body: JSON.stringify(routine)
+    });
+  }
+
+  /* Delete an existing routine */
+  static delete(routine) {
+    return api.fetch(api.routine.url + routine.id, {
+      method: 'DELETE',
+    });
+  }
+
+  /* Retrieve all routines */
+  static getAll() {
+    return api.fetch(api.routine.url);
+  }
+
+  /* Executes a specific routine */
+  // Para que es el body??
+  static exec(routine, param) {
+    return api.fetch(api.routine.url + routine.id + '/execute', {
+      method: 'PUT',
+      headers: {
+        'Content-Type': 'application/json; charset=utf-8'
+      },
+      body: JSON.stringify((param === undefined) ? "{}" : param)
+    });
+  }
+}
+
+/* ---------------------------------------------------------------------- */
 
 Vue.component('toolbar', {
   props: {
@@ -12,8 +234,8 @@ Vue.component('toolbar', {
       active_tab: undefined,
       tabs: [
         { index: 0, name: 'Home', dir: 'home.html' },
-        { index: 1, name: 'Rooms', dir: 'roomSelection.html' },
-        { index: 2, name: 'Devices', dir: 'deviceCategories.html' },
+        { index: 1, name: 'Rooms', dir: 'rooms.html' },
+        { index: 2, name: 'Devices', dir: 'devices.html' },
         { index: 3, name: 'Routines', dir: 'routines.html' }
       ]
     }
@@ -928,78 +1150,78 @@ Vue.component('panel-vacuum', {
 //   console.log('Unexpected error: \n' + error);
 // })
 
-var api = class {
-  static get baseUrl() {
-    return "http://127.0.0.1:8080/api/";
-  }
+// var api = class {
+//   static get baseUrl() {
+//     return "http://127.0.0.1:8080/api/";
+//   }
 
-  static get timeout() {
-    return 10000; // 10 seg
-  }
+//   static get timeout() {
+//     return 10000; // 10 seg
+//   }
 
-  static fetch(url, init) {
-    return new Promise((solve, reject) => {
-      var timeout = setTimeout(() => {
-        reject(new Error('Time out'));
-      }, api.timeout);
+//   static fetch(url, init) {
+//     return new Promise((solve, reject) => {
+//       var timeout = setTimeout(() => {
+//         reject(new Error('Time out'));
+//       }, api.timeout);
 
-      fetch(url, init)
-        .then((response) => {
-          clearTimeout(timeout);
-          if (!response.ok) {
-            reject(new Error(response.statusText));
-          }
-          return response.json();
-        })
-        .then((data) => {
-          solve(data);
-        })
-        .catch((error) => {
-          reject(error);
-        });
-    });
-  }
-}
+//       fetch(url, init)
+//         .then((response) => {
+//           clearTimeout(timeout);
+//           if (!response.ok) {
+//             reject(new Error(response.statusText));
+//           }
+//           return response.json();
+//         })
+//         .then((data) => {
+//           solve(data);
+//         })
+//         .catch((error) => {
+//           reject(error);
+//         });
+//     });
+//   }
+// }
 
-api.room = class {
-  static get url() {
-    return api.baseUrl + "rooms/";
-  }
+// api.room = class {
+//   static get url() {
+//     return api.baseUrl + "rooms/";
+//   }
 
-  static add(room) {
-    return api.fetch(api.room.url, {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json; charset=utf-8'
-      },
-      body: JSON.stringify(room)
-    });
-  }
+//   static add(room) {
+//     return api.fetch(api.room.url, {
+//       method: 'POST',
+//       headers: {
+//         'Content-Type': 'application/json; charset=utf-8'
+//       },
+//       body: JSON.stringify(room)
+//     });
+//   }
 
-  static modify(room) {
-    return api.fetch(api.room.url + room.id, {
-      method: 'PUT',
-      headers: {
-        'Content-Type': 'application/json; charset=utf-8'
-      },
-      body: JSON.stringify(room)
-    });
-  }
+//   static modify(room) {
+//     return api.fetch(api.room.url + room.id, {
+//       method: 'PUT',
+//       headers: {
+//         'Content-Type': 'application/json; charset=utf-8'
+//       },
+//       body: JSON.stringify(room)
+//     });
+//   }
 
-  static delete(id) {
-    return api.fetch(api.room.url + id, {
-      method: 'DELETE',
-    });
-  }
+//   static delete(id) {
+//     return api.fetch(api.room.url + id, {
+//       method: 'DELETE',
+//     });
+//   }
 
-  static get(id) {
-    return api.fetch(api.room.url + id);
-  }
+//   static get(id) {
+//     return api.fetch(api.room.url + id);
+//   }
 
-  static getAll() {
-    return api.fetch(api.room.url);
-  }
-}
+//   static getAll() {
+//     return api.fetch(api.room.url);
+//   }
+// }
 
 Vue.component('add-device', {
 
@@ -1089,7 +1311,7 @@ Vue.component('add-room', {
   template:
     `<v-container fluid>
 
-      <v-overlay :value="overlay">
+      <v-overlay>
       <v-card light>
           <v-card-title>
               <span class="headline">Add Room</span>
@@ -1142,7 +1364,7 @@ Vue.component('add-room', {
 
           <v-card-actions>
               <div class="flex-grow-1"></div>
-              <v-btn color="red darken-1" text @click="overlay = false; snackbarCan = true">Cancel</v-btn>
+              <v-btn color="red darken-1" text @click="cancel()">Cancel</v-btn>
               <v-btn color="green darken-1" text @click="accept()">Create</v-btn>
           </v-card-actions>
       </v-card>
@@ -1158,13 +1380,18 @@ Vue.component('add-room', {
   methods: {
     async accept() {
       // send form to back
-      overlay = false; 
-      snackbarOk = true;
       var aux = await api.room.add({
-        "name": this.room,
-        "image": this.images[this.image],
-        "meta": {}
+        "name": this.name,
+        "meta": {"image": this.images[this.image]}
       }).then(data => data.result);
+      this.overlay = false;
+      this.snackbarOk = true;
+      this.$root.$emit('Finished add');
+    },
+    cancel() {
+      this.overlay = false;
+      this.snackbarCan = true;
+      this.$root.$emit('Finished add');
     }
   },
   mounted: function () {
@@ -1227,7 +1454,7 @@ Vue.component('add-btn', {
   },
   data() {
     return {
-      overlay: false
+      overlay: false,
     }
   },
   template:
@@ -1255,8 +1482,10 @@ Vue.component('add-btn', {
       }
     }
   },
-  mounted: function () {
-    // here we extract all the data
+  mounted() {
+    this.$root.$on('Finished add', () => {
+      this.overlay = false;
+    });
   }
 })
 
